0fbde0c8651865fb484c23083d8525bfc9dacc4f
simhallq <hallqvist.simon@gmail.com>
2022-03-27 16:21:49 +0200

    working

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
+import argparse
 from passet_bot import PassetBot
-bot = PassetBot(start_url='https://bokapass.nemoq.se/Booking/Booking/Index/uppsala', first_name='Simon',
-                last_name='Hällqvist', email='hallqvist.simon@gmail.com', phone='0739609843', latest_date='2019-04-15', driver_path='/Users/simon/Downloads/chromedriver 3')
+
+args = argparse.ArgumentParser()
+args.add_argument('--start_url', '-s', type=str, required=True)
+args.add_argument('--latest_date', '-d', type=str, required=True)
+# parse arguments
+args = args.parse_args()
+# get arguments
+start_url = args.start_url
+latest_date = args.latest_date
+
+bot = PassetBot(start_url=start_url, first_name='Simon',
+                last_name='Hällqvist', email='hallqvist.simon@gmail.com', phone='0739609843', latest_date=latest_date, driver_path='/Users/simon/Downloads/chromedriver 3')
 bot.start_session()
--- src/passet_bot.py
+++ src/passet_bot.py
@@ -13,6 +13,7 @@ class PassetBot:
         self.phone = phone
         self.latest_date = datetime.strptime(latest_date, '%Y-%m-%d')
         self.options = Options()
+        self.options.add_argument('log-level=3')
         self.driver = webdriver.Chrome(executable_path=driver_path, options=self.options)
 
     def start_session(self):
@@ -20,19 +21,17 @@ class PassetBot:
         self.driver.get(self.start_url)
         self._wait()
 
-        # click on "Boka ny tid"
+        # click "Boka ny tid"
         self.driver.find_element_by_name('StartNextButton').click()
         self._wait()
 
         # accept conditions and go to next
         self.driver.find_element_by_xpath('//*[@id="AcceptInformationStorage"]').click()
         self._next_step()
-        self._wait()
 
         # click "jag bor i sverige" and go to next
         self.driver.find_element_by_xpath('//*[@id="ServiceCategoryCustomers_0__ServiceCategoryId"]').click()
         self._next_step()
-        self._wait()
 
         # click "första lediga tid"
         self.driver.find_element_by_xpath('//*[@id="Main"]/form[1]/div/div[6]/div/input[2]').click()
@@ -42,22 +41,48 @@ class PassetBot:
         available_date = datetime.strptime(self.driver.find_element_by_id(
             'datepicker').get_property('value'), '%Y-%m-%d')
         if available_date < self.latest_date:
-            print(f'slot available on {available_date}, booking...')
+            print(f'Slot available on {available_date}, booking...')
             self._book_time()
         else:
-            print('no acceptable dates, retrying in 5 mins')
+            print('No acceptable dates, retrying in 5 mins')
             time.sleep(5 * 60)
             self.start_session()
 
+    def _confirm_booking(self):
+        # enter name and mark "pass" checkbox
+        self.driver.find_element_by_xpath(
+            '//*[@id="Customers_0__BookingFieldValues_0__Value"]').send_keys(self.first_name)
+        self.driver.find_element_by_xpath(
+            '//*[@id="Customers_0__BookingFieldValues_1__Value"]').send_keys(self.last_name)
+        self.driver.find_element_by_xpath('//*[@id="Customers_0__Services_0__IsSelected"]').click()
+        self._next_step()
+        self._next_step()
+        # enter email and phone number
+        self.driver.find_element_by_id('EmailAddress').send_keys(self.email)
+        self.driver.find_element_by_id('ConfirmEmailAddress').send_keys(self.email)
+        self.driver.find_element_by_id('PhoneNumber').send_keys(self.phone)
+        self.driver.find_element_by_id('ConfirmPhoneNumber').send_keys(self.phone)
+        # mark "sms" checkbox
+        self.driver.find_element_by_xpath('//*[@id="SelectedContacts_1__IsSelected"]').click()
+        self.driver.find_element_by_xpath('//*[@id="SelectedContacts_3__IsSelected"]').click()
+        self._next_step()
+        # confirm booking
+        self._next_step()
+        print("Booking successful!")
+        # quit
+        self.driver.quit()
+
     def _next_step(self):
         """
-        Click next button"""
+        Click next button and wait for page to load"""
         self.driver.find_element_by_name("Next").click()
+        self._wait()
 
     def _book_time(self):
         # find timeslot
         self.driver.find_element_by_css_selector("div[class='pointer timecell text-center ']").click()
         self._next_step()
+        self._confirm_booking()
 
     def _reset_session(self):
         self.driver.delete_all_cookies()
