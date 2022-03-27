179ff98b82616ecfcdfc9b1a4e151e1c62451b5a
simhallq <hallqvist.simon@gmail.com>
2022-03-27 17:09:48 +0200

    fix argparse

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 import argparse
 from passet_bot import PassetBot
 
-args = argparse.ArgumentParser()
-args.add_argument('--start_url', '-s', type=str, required=True)
-args.add_argument('--first_name', '-f', type=str, required=True)
-args.add_argument('--last_name', '-l', type=str, required=True)
-args.add_argument('--email', '-e', type=str, required=True)
-args.add_argument('--phone', '-p', type=str, required=True)
-args.add_argument('--latest_date', '-d', type=str, required=True)
-args.add_argument('--driver_path', '-dr', type=str, required=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('--start_url', '-s', type=str, required=True)
+parser.add_argument('--first_name', '-f', type=str, required=True)
+parser.add_argument('--last_name', '-l', type=str, required=True)
+parser.add_argument('--email', '-e', type=str, required=True)
+parser.add_argument('--phone', '-p', type=str, required=True)
+parser.add_argument('--latest_date', '-d', type=str, required=True)
+parser.add_argument('--driver_path', '-dr', type=str, required=True)
+
 # parse arguments
-args = args.parse_args()
+args = vars(parser.parse_args())
 bot = PassetBot(**args)
 bot.start_session()
